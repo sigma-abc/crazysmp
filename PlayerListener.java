9882536050fe44d375b4af687f5c6827e300b56c
Miguel <olayemifatukasi03@gmail.com>
2026-05-01 17:56:47 +0100

    Deleted files

--- PlayerListener.java
+++
@@ -1,9 +0,0 @@
-package dev.crazysmp.listener;
-
-import dev.crazysmp.CrazySMPPlugin;
-import org.bukkit.event.Listener;
-
-public class PlayerListener implements Listener {
-    private final CrazySMPPlugin plugin;
-    public PlayerListener(CrazySMPPlugin plugin) { this.plugin = plugin; }
-}
